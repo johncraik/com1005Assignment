c0187b7a66c0bd718639e6f0fc6d5cd6390fb83f
johncraik <jgcraik1@sheffield.ac.uk>
2021-05-21 19:09:19 +0100

    Changed file structure

--- search3/RunRamblersBB.java
+++ search3/RunRamblersBB.java
@@ -3,15 +3,11 @@ package search3;
 public class RunRamblersBB {
     public static void main(String[] arg) {
 
-        TerrainMap map = new TerrainMap("search3/tmc.pgm");
+        TerrainMap map = new TerrainMap("search3/map1.pgm");
 
-        RamblersSearch searcher = new RamblersSearch(map, new Coords(14, 9));
-        SearchState initState = (SearchState) new RamblersState(new Coords(0, 0), 1);
+        RamblersSearch searcher = new RamblersSearch(map, new Coords(215, 112));
+        SearchState initState = (SearchState) new RamblersState(new Coords(9, 41), 1);
 
-        // change from search1 - specify strategy
-
-         String res_bf = searcher.runSearch(initState, "depthFirst");
-         System.out.println(res_bf);
          String res_bb = searcher.runSearch(initState, "branchAndBound");
          System.out.println(res_bb);
 
