413db4cca1834676921fc86cd8933bd63be25ca8
johncraik <jgcraik1@sheffield.ac.uk>
2021-05-21 19:23:30 +0100

    Switched maps

--- search3/RunRamblersBB.java
+++ search3/RunRamblersBB.java
@@ -3,10 +3,10 @@ package search3;
 public class RunRamblersBB {
     public static void main(String[] arg) {
 
-        TerrainMap map = new TerrainMap("search3/map1.pgm");
+        TerrainMap map = new TerrainMap("tmc.pgm");
 
-        RamblersSearch searcher = new RamblersSearch(map, new Coords(215, 112));
-        SearchState initState = (SearchState) new RamblersState(new Coords(9, 41), 1);
+        RamblersSearch searcher = new RamblersSearch(map, new Coords(15, 15));
+        SearchState initState = (SearchState) new RamblersState(new Coords(0, 0), 1);
 
          String res_bb = searcher.runSearch(initState, "branchAndBound");
          System.out.println(res_bb);
