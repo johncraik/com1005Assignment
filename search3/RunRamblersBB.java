daaa21f71d377b31f707c628d2e9ec20a8622292
johncraik <jgcraik1@sheffield.ac.uk>
2021-05-21 17:57:19 +0100

    Setup the RunRamblersBB class to test out RamblersSearch and RamblersState

--- search3/RunRamblersBB.java
+++ search3/RunRamblersBB.java
@@ -1,5 +1,21 @@
 package search3;
 
 public class RunRamblersBB {
-    
+    public static void main(String[] arg) {
+
+        TerrainMap map = new TerrainMap("search3/tmc.pgm");
+
+        RamblersSearch searcher = new RamblersSearch(map, new Coords(14, 9));
+        SearchState initState = (SearchState) new RamblersState(new Coords(0, 0), 1);
+
+        // change from search1 - specify strategy
+
+         String res_bf = searcher.runSearch(initState, "depthFirst");
+         System.out.println(res_bf);
+         String res_bb = searcher.runSearch(initState, "branchAndBound");
+         System.out.println(res_bb);
+
+        //String res_astar = searcher.runSearch(initState, "AStar");
+        //System.out.println(res_astar);
+    }
 }
